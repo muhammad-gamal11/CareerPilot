7664a777312b6af05df8ff2fac09d6ce2a127e96
Muhammad Gamal <gamal9978@gmail.com>
2024-03-08 21:50:23 +0200

    done setting the submit functionality

---
+++ src/components/FormRow.js
@@ -0,0 +1,17 @@
+const FormRow = ({ type, name, value, handleChange, labelText }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {name}
+      </label>
+      <input
+        type={type}
+        name={name}
+        className="form-input"
+        onChange={handleChange}
+        value={value}
+      />
+    </div>
+  );
+};
+export default FormRow;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,75 @@
-const Resigter = () => {
-  return <div>Resigter</div>;
+import { useEffect, useState } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { FormRow, Logo } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
 };
-export default Resigter;
+
+const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      console.log("type");
+    }
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        {/* single form */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* single form end */}
+        {/* single form */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        {/* single form end */}
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+export default Register;
