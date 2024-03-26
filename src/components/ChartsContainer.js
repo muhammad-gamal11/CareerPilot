3571b86b9687f5a762c2d85566cb9b7643afc402
Muhammad Gamal <gamal9978@gmail.com>
2024-03-26 02:33:55 +0200

    removed area chart

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,20 +1,14 @@
-import React, { useState } from "react";
-
 import BarChart from "./BarChart";
-import AreaChart from "./AreaChart";
 import Wrapper from "../assets/wrappers/ChartsContainer";
 import { useSelector } from "react-redux";
 
 const ChartsContainer = () => {
-  const [barChart, setBarChart] = useState(true);
   const { monthlyApplications: data } = useSelector((store) => store.allJobs);
   return (
     <Wrapper>
       <h4>Monthly Applications</h4>
-      <button type="button" onClick={() => setBarChart(!barChart)}>
-        {barChart ? "Area Chart" : "Bar Chart"}
-      </button>
-      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+      <button type="button">Bar Chart</button>
+      <BarChart data={data} />
     </Wrapper>
   );
 };
